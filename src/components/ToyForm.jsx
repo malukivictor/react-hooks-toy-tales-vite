40f18e146e098ff1cc22a7458daf95796ede8d54
Maluki Victor <victormaluki0@gmail.com>
2026-09-01 18:42:22 +0300

    test 4 run 2.1

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ToyForm({ onAddNewToy }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    const newToy = {
-      name: e.target.name.value,
-      image: e.target.image.value,
-      likes: 0,
-    };
-    onAddNewToy(newToy);
-    e.target.reset();
+
+    const newToy = { name, image, likes: 0 };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newToy),
+    })
+      .then((res) => res.json())
+      .then((toyFromServer) => {
+        onAddNewToy(toyFromServer);
+        setName("");
+        setImage("");
+      });
   }
 
   return (
@@ -21,6 +31,8 @@ function ToyForm({ onAddNewToy }) {
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <br />
         <input
@@ -28,6 +40,8 @@ function ToyForm({ onAddNewToy }) {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
         />
         <br />
         <input
